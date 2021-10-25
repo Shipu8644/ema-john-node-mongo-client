266873d039d94651665ecd905fc61e0bdbeb2659
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-25 16:56:28 +0600

    first commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyBYJyDQnjyXzCmDl80Yuk5FIoFGy_7vRS4",
+    authDomain: "ema-john-simple-784b5.firebaseapp.com",
+    projectId: "ema-john-simple-784b5",
+    storageBucket: "ema-john-simple-784b5.appspot.com",
+    messagingSenderId: "749911981135",
+    appId: "1:749911981135:web:053ba565959a087546be3a"
 };
 
 export default firebaseConfig;
