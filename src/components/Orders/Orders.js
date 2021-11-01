5239b969c2b083066c3dd14dcdb5633c5cb6f4cd
Shipu8644 <shipu.monjurur@gmail.com>
2021-11-02 00:23:32 +0600

    verify token

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import useAuth from '../../hooks/useAuth';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const { user } = useAuth();
+    const history = useHistory();
 
     useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user.email}`, {
@@ -11,7 +13,14 @@ const Orders = () => {
                 "authorization": `Bearer ${localStorage.getItem(`idToken`)}`
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                else if (res.status === 401) {
+                    history.push('/login');
+                }
+            })
             .then(data => setOrders(data))
     }, [])
     return (
