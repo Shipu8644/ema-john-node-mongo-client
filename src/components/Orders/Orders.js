3c015661262cee9a3037984b94fe52ac38690564
Shipu8644 <shipu.monjurur@gmail.com>
2021-11-01 13:09:52 +0600

    query email done orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 
 const Orders = () => {
+    const [orders, setOrders] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [])
     return (
         <div>
-            <h2>This is orders page</h2>
+            <h2>You have placed {orders.length} orders </h2>
+            <ul>
+                {orders.map(order => <li
+                    key={order._id}
+                >{order.name} : {order.email}</li>)}
+            </ul>
         </div>
     );
 };
